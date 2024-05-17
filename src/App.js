470af455e09bbb0d11175d00dc24d74a6b22c037
JozefK <164421631+jozefkubis@users.noreply.github.com>
2024-05-17 20:33:48 +0200

    Dokoncenie statickej casti app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="caluculator">
+    <div className="calculator">
       <Display />
       <Buttons />
     </div>
@@ -8,9 +8,36 @@ export default function App() {
 }
 
 function Display() {
-  return <div className="display"></div>
+  return (
+    <div className="display">
+      <input type="text" />
+    </div>
+  )
 }
 
 function Buttons() {
-  return <div className="buttons"></div>
+  return (
+    <div className="buttons">
+      <button className="button">C</button>
+      <button className="button">()</button>
+      <button className="button">/</button>
+      <button className="button">8</button>
+      <button className="button">X</button>
+      <button className="button">9</button>
+      <button className="button">4</button>
+      <button className="button">%</button>
+      <button className="button">5</button>
+      <button className="button">7</button>
+      <button className="button">6</button>
+      <button className="button">-</button>
+      <button className="button">1</button>
+      <button className="button">2</button>
+      <button className="button">3</button>
+      <button className="button">+</button>
+      <button className="button">+/-</button>
+      <button className="button">0</button>
+      <button className="button">,</button>
+      <button className="button">=</button>
+    </div>
+  )
 }
